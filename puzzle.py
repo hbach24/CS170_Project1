f660611b350288b97b9eb7c94ec7126d78ab812a
hbach24 <57569284+hbach24@users.noreply.github.com>
2022-10-29 14:06:52 -0700

    finished one operation: move_up and working on uniform cost search

--- puzzle.py
+++ puzzle.py
@@ -1,12 +1,15 @@
 import heapq
+import queue
+import copy
 '''
 Problem space:
 Operators: move blank left, right, up, or down
 
 '''
 #GOOD REFERENCE: SLide 24 & 25 for Week 1 Heuristic Search Slides
-default = [[1, 2, 3], [5, 0, 6], [4, 7, 8]] 
+default = [[1, 2, 3], [5, 0, 6], [4, 7, 8]]  #depth = 4 according to project instructions
 goal = [[1, 2, 3], [4, 5, 6], [7, 8 , 0]]
+N = 3 #size of NxN matrix
 #the DEPTH of the tree at which we find a solution is the COST for that solution (Blind Search Part 1 Slide 28)
 
 class Node:
@@ -15,6 +18,7 @@ class Node:
        self.h = h #h should be set to 0 for Uniform Cost Search
        self.g = g #depth of the node
        self.f = self.h + self.g
+       self.children = []
 
 def main():
     print("Welcome to Hannah Bach's 8-Puzzle Solver!\n")
@@ -30,7 +34,7 @@ def main():
     if(choice == '1'):
         # UniformCostSearch()
         root = Node(problem, 0, 0, 0)
-        generalSearch(root)
+        UniformCostSearch(root)
     if(choice == '2'):
         misplaced()
     if(choice == '3'):
@@ -49,9 +53,9 @@ def createPuzzle():
     print("\nTo create your puzzle, please input the number of each tile row-wise. Make sure to press 'enter' after inputting each number.")
     newPuzzle = []
 
-    for i in range(3):
+    for i in range(N):
         row = []
-        for j in range(3):
+        for j in range(N):
             tile = int(input())
             row.append(tile)
         newPuzzle.append(row)
@@ -59,28 +63,70 @@ def createPuzzle():
     print(newPuzzle)
     return newPuzzle
 
-def generalSearch(root):
-    nodes = [] #queue for all the child nodes
-    print("ROOT", root.state)
-    heapq.heappush(nodes, (root.f, root)) 
+
+def moveUp(currNode, blankRow, blankCol):
+    childNode = copy.deepcopy(currNode)
+    childNode.state[blankRow][blankCol] = currNode.state[blankRow-1][blankCol]
+    childNode.state[blankRow-1][blankCol] = 0
+    childNode.g = currNode.g + 1
+    childNode.h = 0; #change this later
+    childNode.f = childNode.g + childNode.h
+    # print("NEW CHILDNODE: ", childNode.state, childNode.g, childNode.h, childNode.f)
+    return childNode
+    
+
+def expand(currNode):
+    #find the blank space/zero 
+    for i in range(N):
+        for j in range(N):
+            if(currNode.state[i][j] == 0):
+                blankRow = i 
+                blankCol = j
+                print("Row, Col: ", blankRow, blankCol)
+
+    # (1) expand the node's children by performing the valid operations: move_up, move_down, move_left, move_right
+    # (2) add the node's children to the curr node's curr.children list and return its list so we can add it to the queue when we return to UCS
+    # NOTE: set the children's g value to 1 (depth = 1)
+
+    #if located at corners, we only perform 2 moves
+    if(blankRow != 0):
+        print("enter")
+        childNode = moveUp(currNode, blankRow, blankCol)
+        currNode.children.append(childNode) #append the newly expanded/generated child node after performing a move_up operation to the current node's list of children
+
+
+
+    print("exp")
+
+def UniformCostSearch(root):
+    nodes = queue.Queue() #queue for all the child nodes
+    visited = []
+    print("ROOT", root.state) 
+
+    # heapq.heappush(nodes, root) 
+    nodes.put(root)
     while(1):
-        if(len(nodes) == 0):
+       
+        if(nodes.qsize() == 0):
             print("Failed to find the solution.")
             return False
-        currNode = heapq.heappop(nodes) 
-        # print(currNode, currNode[0], currNode[1].state)
-        if(checkGoal(currNode[1].state)):
-            return currNode[1].state;
+        currNode = nodes.get()
+        if(checkGoal(currNode)):
+            # print("SIZE", nodes.qsize(), currNode)
+            print("Goal found!")
+            return currNode;
         else:
-            print("EXPAND currNode's children and add them to the priority queue")
-        return True;
+            if(currNode.state not in visited):
+                expand(currNode)
+                print(currNode.state)
+                visited.append(currNode.state) #add currNode's state to visited list so we don't expand it again
+                print("EXPAND currNode's children and add them to the queue") 
+        # print("visited", visited)
+    
+    return True; 
+        #TODO: https://stackoverflow.com/questions/71877490/checking-already-visited-states-in-a-solution-for-8-puzzle-problem
         
-
-def expand():
-    print("expand")
-
-def UniformCostSearch():
-    print("UCS")
+#when expanding a node, only add children nodes that haven't been visited yet
 
 def misplaced():
     print("A* with the Misplaced Tile heuristic")
@@ -90,11 +136,12 @@ def manhattan():
 
 def checkGoal(currNode):
     #TODO: check if the node passed in here is equivalent to the goal state
-
-
-
-
-
+    if(currNode.state == goal):
+        print("curr:", currNode.state)
+        print("goal:", goal)
+        return 1
+    else:
+        return 0 
 
 if __name__ == '__main__':
     main()
